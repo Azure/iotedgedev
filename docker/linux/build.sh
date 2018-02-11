4e524fd2d618c35ddfeb4b12d34350cffa57bea2
Jon Gallant <jon@jongallant.com>
2018-02-10 23:08:11 -0800

    Merge -dev with -deps docker

--- docker/linux/build.sh
+++ docker/linux/build.sh
@@ -2,5 +2,3 @@ export VERSION=$1
 docker build -f Dockerfile.deps -t jongallant/iotedgedev-deps:$1 -t jongallant/iotedgedev-deps:$1-linux -t jongallant/iotedgedev-deps:latest -t jongallant/iotedgedev-deps:latest-linux   . 
 cat Dockerfile | envsubst > Dockerfile.expanded
 docker build -f Dockerfile.expanded -t jongallant/iotedgedev:$1 -t jongallant/iotedgedev:$1-linux -t jongallant/iotedgedev:latest -t jongallant/iotedgedev:latest-linux .
-cat Dockerfile.dev | envsubst > Dockerfile.dev.expanded
-docker build -f Dockerfile.dev.expanded -t jongallant/iotedgedev-dev:$1 -t jongallant/iotedgedev-dev:$1-linux -t jongallant/iotedgedev-dev:latest -t jongallant/iotedgedev-dev:latest-linux .
--- docker/linux/run-dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v c:/temp/iotedge:/iotedge jongallant/iotedgedev-dev:$1-linux
