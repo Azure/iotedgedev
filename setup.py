87ad41cd4a538ae8f4dc8b4e94dfdb90a591b85c
Jon Gallant <jonbgallant@users.noreply.github.com>
2018-04-17 19:00:31 -0700

    Bump version to 81 (#156)
    * Readme improvements.
    
    * Bump version: 0.79.0 → 0.80.0
    
    * Bump version: 0.80.0 → 0.81.0

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ test_requirements = [
 
 setup(
     name='iotedgedev',
-    version='0.79.0',
+    version='0.81.0',
     description="The Azure IoT Edge Dev Tool greatly simplifies the IoT Edge development process by automating many routine manual tasks, such as building, deploying, pushing modules and configuring the IoT Edge Runtime.",
     long_description="See https://github.com/azure/iotedgedev for usage instructions.",
     author="Jon Gallant",
