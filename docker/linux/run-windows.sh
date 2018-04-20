f71d699971923511a28e0812f2d763ac9367f394
Davide Mauri <damauri@microsoft.com>
2018-04-20 16:46:43 -0700

    automatically map folder

--- docker/linux/run-windows.sh
+++ docker/linux/run-windows.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-folder=$(echo $PWD | cut -d/ -f-6)
+folder=$(echo $PWD | cut -d/ -f-6) | sed -e 's:/c:c\::g' | sed -e 's:/:\\:g'
 
-docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v $folder:/iotedge microsoft/iotedgedev:latest-linux
+docker run -it -v $folder:/home/iotedge/tool microsoft/iotedgedev:latest-linux
