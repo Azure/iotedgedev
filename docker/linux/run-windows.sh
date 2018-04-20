27505a68cc25f0bfd75392852f42f89174ddffd2
Davide Mauri <damauri@microsoft.com>
2018-04-20 16:49:58 -0700

    added sock mapping

--- docker/linux/run-windows.sh
+++ docker/linux/run-windows.sh
@@ -2,4 +2,4 @@
 
 folder=$(echo $PWD | cut -d/ -f-6) | sed -e 's:/c:c\::g' | sed -e 's:/:\\:g'
 
-docker run -it -v $folder:/home/iotedge/tool microsoft/iotedgedev:latest-linux
+docker run -it -v /var/run/docker.sock:/var/run/docker.sock -v $folder:/home/iotedge/tool microsoft/iotedgedev:latest-linux
