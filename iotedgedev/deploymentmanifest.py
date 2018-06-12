e8c03a93414bd5cc3b997f35ce77477aba4c3a45
Ray Fang <fangzh@microsoft.com>
2018-06-12 14:47:17 +0800

    Add docstring for deploymentmanifest

--- iotedgedev/deploymentmanifest.py
+++ iotedgedev/deploymentmanifest.py
@@ -1,4 +1,10 @@
+"""
+This module provides interfaces to manipulate IoT Edge deployment manifest (deployment.json)
+and deployment manifest template (deployment.template.json)
+"""
+
 import json
+
 
 class DeploymentManifest:
     def __init__(self, path, is_template):
@@ -7,6 +13,7 @@ class DeploymentManifest:
         self.is_template = is_template
 
     def add_module_template(self, module_name):
+        """Add a module template to the deployment manifest with amd64 as the default platform"""
         new_module = """{
             "version": "1.0",
             "type": "docker",
@@ -21,5 +28,6 @@ class DeploymentManifest:
         self.json["moduleContent"]["$edgeAgent"]["properties.desired"]["modules"][module_name] = json.loads(new_module)
 
     def save(self):
+        """Dump the JSON to the disk"""
         with open(self.path, "w") as deployment_manifest:
             json.dump(self.json, deployment_manifest, indent=2)
