97ddbeee9735ebecbfe8a343291fe5afa5dacfce
Ray Fang <fangzh@microsoft.com>
2018-07-18 23:24:26 +0800

    Add unit test for utility.py

---
+++ tests/test_utility.py
@@ -0,0 +1,50 @@
+import json
+import os
+
+import pytest
+
+from iotedgedev.envvars import EnvVars
+from iotedgedev.output import Output
+from iotedgedev.utility import Utility
+
+pytestmark = pytest.mark.unit
+
+tests_dir = os.path.join(os.getcwd(), "tests")
+test_assets_dir = os.path.join(tests_dir, "assets")
+test_file_1 = os.path.join(test_assets_dir, "deployment.template_1.json")
+test_file_2 = os.path.join(test_assets_dir, "deployment.template_2.json")
+
+
+@pytest.fixture
+def utility():
+    output = Output()
+    envvars = EnvVars(output)
+    envvars.load()
+    return Utility(envvars, output)
+
+
+def test_copy_template(utility, tmpdir):
+    replacements = {
+        "${MODULES.csharpmodule.amd64}": "localhost:5000/csharpmodule:0.0.1-amd64",
+        "${MODULES.csharpfunction.amd64.debug}": "localhost:5000/csharpfunction:0.0.1-amd64.debug"
+    }
+    dest = tmpdir.join("deployment_template_1.dest.json")
+    utility.copy_template(test_file_1, dest, replacements=replacements, expand_env=False)
+    assert_json_file_equal(test_file_2, dest)
+
+
+def test_copy_template_expand_env(utility, tmpdir):
+    replacements = {
+        "${MODULES.csharpmodule.amd64}": "${CONTAINER_REGISTRY_SERVER}/csharpmodule:0.0.1-amd64",
+        "${MODULES.csharpfunction.amd64.debug}": "${CONTAINER_REGISTRY_SERVER}/csharpfunction:0.0.1-amd64.debug"
+    }
+    os.environ["CONTAINER_REGISTRY_SERVER"] = "localhost:5000"
+    dest = tmpdir.join("deployment_template_2.dest.json")
+    utility.copy_template(test_file_1, dest, replacements=replacements, expand_env=True)
+    assert_json_file_equal(test_file_2, dest)
+
+
+def assert_json_file_equal(file1, file2):
+    with open(file1, "r") as f1:
+        with open(file2, "r") as f2:
+            assert json.load(f1) == json.load(f2)
