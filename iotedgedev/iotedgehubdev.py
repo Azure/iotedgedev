63f36b988275bf295e66f3349ca59868848508e1
Ray Fang <fangzh@microsoft.com>
2018-08-01 17:03:40 +0800

    Allow start iotedgehubdev in verbose mode

--- iotedgedev/iotedgehubdev.py
+++ iotedgedev/iotedgehubdev.py
@@ -17,14 +17,14 @@ class iotedgehubdev:
         self.output.header("Starting Edge Simulator in Single Mode")
         self.utility.call_proc("iotedgehubdev start -i {0}".format(inputs).split())
 
-    def start_solution(self, no_build=False):
+    def start_solution(self, verbose=True, no_build=False):
         if not no_build:
             dock = Docker(self.envvars, self.utility, self.output)
             mod = Modules(self.envvars, self.utility, self.output, dock)
             mod.build()
 
         self.output.header("Starting Edge Simulator in Solution Mode")
-        self.utility.call_proc("iotedgehubdev start -d {0} -v".format(self.envvars.DEPLOYMENT_CONFIG_FILE_PATH).split())
+        self.utility.call_proc("iotedgehubdev start -d {0} {1}".format(self.envvars.DEPLOYMENT_CONFIG_FILE_PATH, "-v" if verbose else "").split())
 
     def stop(self):
         self.output.header("Stopping Edge Simulator")
