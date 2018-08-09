e026efd17059f1c3311b55c0962451a640b13a52
Ray Fang <fangzh@microsoft.com>
2018-08-09 22:12:31 +0800

    Add tests

---
+++ tests/test_config.py
@@ -0,0 +1,27 @@
+import os
+
+import pytest
+
+from iotedgedev.config import Config
+
+pytestmark = pytest.mark.unit
+
+
+def test_firsttime(request):
+    config = Config()
+
+    def clean():
+        config_path = config.get_config_path()
+        if os.path.exists(config_path):
+            os.remove(config_path)
+    request.addfinalizer(clean)
+
+    clean()
+
+    assert config.get(config.DEFAULT_DIRECT, config.FIRSTTIME_SECTION) == 'yes'
+    assert config.get(config.DEFAULT_DIRECT, config.TELEMETRY_SECTION) is None
+
+    config.check_firsttime()
+
+    assert config.get(config.DEFAULT_DIRECT, config.FIRSTTIME_SECTION) == 'no'
+    assert config.get(config.DEFAULT_DIRECT, config.TELEMETRY_SECTION) == 'yes'
