7ffc52900650a698102505c6a16f5011a7ef327f
Ray Fang <fangzh@microsoft.com>
2018-08-11 00:59:50 +0800

    Fix unfinished conflict resolve

--- iotedgedev/modules.py
+++ iotedgedev/modules.py
@@ -146,7 +146,7 @@ class Modules:
                     response = docker.docker_client.images.push(repository=tag, stream=True, auth_config={
                         "username": self.envvars.CONTAINER_REGISTRY_USERNAME,
                         "password": self.envvars.CONTAINER_REGISTRY_PASSWORD})
-                    self.dock.process_api_response(response)
+                    docker.process_api_response(response)
             self.output.footer("BUILD COMPLETE", suppress=no_build)
             self.output.footer("PUSH COMPLETE", suppress=no_push)
         self.utility.set_config(force=True, replacements=replacements)
