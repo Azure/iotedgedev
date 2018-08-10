f0dedcd486e0ffcaf7bd3906ea59f5d997a9a8f3
Davide Mauri <info@davidemauri.it>
2018-08-10 14:29:44 -0700

    Added support for windows containers via nanoserver (#158)
    * added initial support for windows containers
    
    * initial support for nanoserver
    
    * corrected az cli path
    
    * only az not working
    
    * all dependencies correctly working
    
    * added required pip packages
    
    * removed unneeded file
    
    * removed external dependencies
    
    * completed support for nanoserver
    
    * fixed all issued mentioned in PR comments
    
    * installing both python2 and python3 on nanoserver
    
    * set path to point to python3, since python2 is not working yet
    
    * added missing parenthesis
    
    * set python3 as default python
    
    * mapped folder set automatically
    
    * improved build script
    
    * added setuptools install
    
    * set to use latest tag
    
    * added support for powershell script
    
    * automatically map folder
    
    * added sock mapping
    
    * removed pip from requirements since it may generate errors
    
    * ignored .backup files
    
    * fixed spacings
    
    * install modules locally
    
    * fixed bug the prevented arguments to be handled correctly
    
    * fixed bug the prevented arguments to be handled correctly
    
    * added build script alpha version
    
    * comments cleanup
    
    * build will now also push docker images
    
    * Bump version: 0.86.0 → 0.87.0
    
    * version set back to 0.81.0
    
    * fixed some bugs, added parameter for pipy login
    
    * modularized build script
    
    * fixed syntax
    
    * removed renaming of python3 executables
    
    * switch to docker folder if needed
    
    * exit on error
    
    * correctly handled directory switching
    
    * switched to ubuntu 18.04 for python 3.6 default support
    
    * using double slash to make script work
    
    * improved docker image build performances
    
    * file system cleanup
    
    * added platform parameter

---
+++ docker/build-docker.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+
+# stop on error
+set -e
+
+# make sure we're in docker folder
+original_folder=$PWD
+
+if [ ! -z "$(echo $PWD | grep /docker$)" ]; then 
+    in_docker_folder=1
+else
+    in_docker_folder=0
+    cd docker
+fi
+
+# read IoTEdgeDev version from python __init__ file
+export VERSION=$(cat ../iotedgedev/__init__.py | grep '__version__' | grep -oP "'\K[^']+")
+
+PYTHON2="2.7.14" #TODO READ FROM deps.txt
+PYTHON3="3.6.5"
+
+build_linux=1
+build_windows=1
+
+function switch_docker
+{
+    echo "===== Switching Docker engine"
+    echo "===== From: " $(docker version --format '{{.Server.Os}}')
+    /c/Program\ Files/Docker/Docker/DockerCli.exe -SwitchDaemon
+    echo "===== To: " $(docker version --format '{{.Server.Os}}')
+}
+
+function get_docker_mode
+{
+    echo $(docker version --format '{{.Server.Os}}')
+}
+
+function check_docker_expected_mode 
+{
+    local mode=$(get_docker_mode)    
+
+    if [ $mode != $1 ]; then
+        echo "===== ERROR: docker is not in expected mode: '$1'"
+        exit 1
+    fi    
+}
+
+function build_linux
+{
+    echo "===== Building Linux Based images"
+
+    check_docker_expected_mode "linux"
+
+    cd linux
+
+    rm iotedgedev-$VERSION-py2.py3-none-any.whl --force
+    
+    cp ../../dist/iotedgedev-$VERSION-py2.py3-none-any.whl iotedgedev-$VERSION-py2.py3-none-any.whl
+
+    docker build \
+        -f Dockerfile.base \
+        -t iotedgedev-linux-base \
+        .
+
+    docker build \
+        -f Dockerfile \
+        -t microsoft/iotedgedev:$VERSION-linux \
+        -t microsoft/iotedgedev:latest-linux \
+        --build-arg IOTEDGEDEV_VERSION=$VERSION \
+        .
+
+    rm iotedgedev-$VERSION-py2.py3-none-any.whl --force
+
+    cd ..
+}
+
+function build_windows
+{
+    echo "===== Building Windows Based images"
+    
+    check_docker_expected_mode "windows"
+
+    cd windows
+
+    rm iotedgedev-$VERSION-py2.py3-none-any.whl --force
+    
+    cp ../../dist/iotedgedev-$VERSION-py2.py3-none-any.whl iotedgedev-$VERSION-py2.py3-none-any.whl
+
+    docker build \
+        -f Dockerfile.base \
+        -t iotedgedev-windows-base \
+        --build-arg PYTHON2_VERSION=$PYTHON2 \
+        --build-arg PYTHON3_VERSION=$PYTHON3 \
+        .
+        
+    docker build \
+        -f Dockerfile \
+        --build-arg IOTEDGEDEV_VERSION=$VERSION \
+        -t microsoft/iotedgedev:$VERSION-windows \
+        -t microsoft/iotedgedev:latest-windows \
+        .
+
+    rm iotedgedev-$VERSION-py2.py3-none-any.whl --force
+
+    cd ..
+}
+
+if [ ! -z "$1" ];  then
+    if [ "$1" = "--help" ]; then    
+        echo "Usage:"
+        echo "build-docker.sh [linux|windows]"
+        exit 1
+    fi
+
+    if [ "$1" = "linux" ]; then
+        build_windows=0
+        echo "===== Building Linux image only"
+    elif [ "$1" = "windows" ]; then
+        build_linux=0
+        echo "===== Building Windows image only"
+    else
+        echo "Unknown option: $1"
+        echo "Use --help for help"
+        exit 1
+    fi
+else
+    echo "===== Building Windows and Linux images"
+fi
+
+mode=$(get_docker_mode)
+echo "===== Docker is in '$mode' container mode"
+if [ $mode = "windows" ]; then
+    # Docker is in Windows Container mode
+    if [ $build_windows = "1" ]; then
+        build_windows
+    fi
+    if [ $build_linux = "1" ]; then
+        switch_docker
+        build_linux
+        switch_docker
+    fi
+else
+    # Docker is in Linux Container mode
+    if [ $build_linux -eq "1" ]; then    
+        build_linux
+    fi
+    if [ $build_windows -eq "1" ]; then    
+        switch_docker
+        build_windows
+        switch_docker
+    fi
+fi
+
+if [ in_docker_folder = 0 ]; then
+    cd original_folder
+fi
--- docker/linux/push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-winpty docker login
-docker push microsoft/iotedgedev:$1
-docker push microsoft/iotedgedev:$1-linux
-docker push microsoft/iotedgedev:latest
-docker push microsoft/iotedgedev:latest-linux
