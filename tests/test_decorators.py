f4dfb5757b4c87910caac150ee8d5c915d0e0dca
w12379564 <imganyuxiang@gmail.com>
2018-08-24 15:32:22 +0800

    add tests for hash256_result, refine tests for suppress_all_exceptions

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,32 +1,44 @@
 import pytest
+import hashlib
 
 from iotedgedev.decorators import suppress_all_exceptions
+from iotedgedev.decorators import hash256_result
 
 pytestmark = pytest.mark.unit
 
 
-@pytest.fixture
-def error_function():
-    def _error_function(exception, fallback_return):
-        @suppress_all_exceptions(fallback_return=fallback_return)
-        def _inner_error_function(exception):
-            if not exception:
-                return 'Everything is OK'
-            else:
-                raise exception()
-        return _inner_error_function(exception)
-    return _error_function
+def test_suppress_all_exceptions():
+    @suppress_all_exceptions()
+    def test_valid():
+        return 'Everything is OK'
+    assert test_valid() == 'Everything is OK'
+
+    @suppress_all_exceptions('fallback')
+    def test_exception_fallback():
+        raise Exception
+    assert test_exception_fallback() == 'fallback'
+
+    @suppress_all_exceptions()
+    def test_exception_nofallback():
+        raise Exception
+    assert not test_exception_nofallback()
 
 
-def test_suppress_all_exceptions(error_function):
-    err_fn = error_function(Exception, 'fallback')
-    assert err_fn == 'fallback'
+def test_hash256_result():
+    @hash256_result
+    def test_none():
+        return None
+    with pytest.raises(ValueError):
+        test_none()
 
-    err_fn = error_function(None, 'fallback')
-    assert err_fn == 'Everything is OK'
+    @hash256_result
+    def test_nostring():
+        return 0
+    with pytest.raises(ValueError):
+        test_nostring()
 
-    err_fn = error_function(ImportError, 'fallback for ImportError')
-    assert err_fn == 'fallback for ImportError'
-
-    err_fn = error_function(None, None)
-    assert err_fn == 'Everything is OK'
+    @hash256_result
+    def test_valid():
+        return "test"
+    expect_hash = hashlib.sha256("test".encode('utf-8'))
+    assert str(expect_hash.hexdigest()) == test_valid()
