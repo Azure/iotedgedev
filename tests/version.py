030762f2f327a8ba5d6fb9ee2897e4e3c4737cbb
Spencer McDonough <shmcdono@usc.edu>
2021-01-08 09:38:20 -0800

    test version assertion error in python2 environment

--- tests/version.py
+++ tests/version.py
@@ -4,3 +4,4 @@ import sys
 test_py2 = sys.version_info < (3, 0)
 
 minversion = pytest.mark.skipif(test_py2, reason="Python 2 not supported. Skipping.")
+py2_test = pytest.mark.skipif(not test_py2, reason="Python 3 supported. Skipping.")
