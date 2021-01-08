0ed088f84a54e0ab161a5a5dd3eeb8f4695af5e7
Spencer McDonough <shmcdono@usc.edu>
2021-01-08 10:20:29 -0800

    fix syntax error

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,5 +6,5 @@ pytestmark = pytest.mark.e2e
 
 @py2_test
 def test_version_assertion_error():
-    with pytest.raises(AssertionError, match="Python 2 is no longer supported on this project. Please upgrade to Python 3.6 or higher.")
+    with pytest.raises(AssertionError, match="Python 2 is no longer supported on this project. Please upgrade to Python 3.6 or higher."):
         from iotedgedev import version
