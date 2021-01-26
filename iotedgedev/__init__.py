2e7c18dca6b1b8752fc0b0f913a835636818994d
Spencer McDonough <shmcdono@usc.edu>
2021-01-26 11:11:17 -0800

    add msft open source metadata

--- iotedgedev/__init__.py
+++ iotedgedev/__init__.py
@@ -1,6 +1,6 @@
 """Top-level package for Azure IoT Edge Dev Tool."""
 
-__author__ = 'Microsoft Corporation'
-__email__ = 'vsciet@microsoft.com'
+__author__ = 'Microsoft Open Source'
+__email__ = 'opencode@microsoft.com'
 __version__ = '3.0.0'
 __AIkey__ = '95b20d64-f54f-4de3-8ad5-165a75a6c6fe'
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 """Test package for Azure IoT Edge Dev Tool ."""
 
-__author__ = 'David Ruttka'
-__email__ = 'david.ruttka@microsoft.com'
-__version__ = '0.0.1'
+__author__ = 'Microsoft Open Source'
+__email__ = 'opencode@microsoft.com'
+__version__ = '3.0.0'
