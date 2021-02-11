ecb9b2cc7cf395eb3f089817bd2376c967ed84d5
Spencer McDonough <shmcdono@usc.edu>
2021-02-11 11:22:49 -0800

    simplify

--- runtime/arm/runtime_tool.arm32v7.sh
+++ runtime/arm/runtime_tool.arm32v7.sh
@@ -1,26 +1,11 @@
-apt-get update && apt-get install -y --no-install-recommends \
-apt-transport-https \
-ca-certificates \
-curl \
-iptables \
-iproute2 \
-libcurl3 \
-libffi-dev \
-libssl1.0.0=1.0.2g-1ubuntu4.13 \
-libssl-dev=1.0.2g-1ubuntu4.13 \
-systemd && \
-rm -rf /var/lib/apt/lists/*
-
-apt-get install moby-engine moby-cli
-
-apt-get update && apt-get install -f
+apt-get update && apt-get install curl
 
 curl https://packages.microsoft.com/config/debian/stretch/multiarch/prod.list > ./microsoft-prod.list
 sudo cp ./microsoft-prod.list /etc/apt/sources.list.d/
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo cp ./microsoft.gpg /etc/apt/trusted.gpg.d/
 
-apt-get update && apt-get install -f
+apt-get install moby-engine moby-cli
 
 apt-get install iotedge
 
@@ -28,12 +13,3 @@ cp ../../docker/runtime/arm/rund.arm32v7.sh rund.sh
 
 sed -i 's/\r//' ./rund.sh && \
 chmod u+x rund.sh
-
-# cd /lib/arm-linux-gnueabihf/
-
-# ln -s libcrypto.so.1.0.0 libcrypto.so.1.0.2 && \
-# ln -s libssl.so.1.0.0 libssl.so.1.0.2
-
-# cd /
-
-# bash rund.sh
