7b0907428cb8d8a3dd473eb0ad9c578057ebc65a
Spencer McDonough <shmcdono@usc.edu>
2021-02-10 17:23:52 -0800

    try apt-get

--- runtime/arm/runtime_tool.arm32v7.sh
+++ runtime/arm/runtime_tool.arm32v7.sh
@@ -11,15 +11,19 @@ libssl-dev=1.0.2g-1ubuntu4.13 \
 systemd && \
 rm -rf /var/lib/apt/lists/*
 
-wget https://aka.ms/moby-engine-armhf-latest -o moby_engine.deb && dpkg -i ./moby_engine.deb && rm ./moby_engine.deb && \
-wget https://aka.ms/moby-cli-armhf-latest -o moby_cli.deb && dpkg -i ./moby_cli.deb && rm ./moby_cli.deb
+# wget https://aka.ms/moby-engine-armhf-latest -o moby_engine.deb && dpkg -i ./moby_engine.deb && rm ./moby_engine.deb && \
+# wget https://aka.ms/moby-cli-armhf-latest -o moby_cli.deb && dpkg -i ./moby_cli.deb && rm ./moby_cli.deb
+
+# apt-get update && apt-get install -f
+
+# wget https://aka.ms/libiothsm-std-linux-armhf-latest -o libiothsm-std.deb && dpkg -i ./libiothsm-std.deb && rm ./libiothsm-std.deb && \
+# wget https://aka.ms/iotedged-linux-armhf-latest -o iotedge.deb && dpkg -i ./iotedge.deb && rm ./iotedge.deb
+
+apt-get install moby-engine moby-cli
 
 apt-get update && apt-get install -f
 
-wget https://aka.ms/libiothsm-std-linux-armhf-latest -o libiothsm-std.deb && dpkg -i ./libiothsm-std.deb && rm ./libiothsm-std.deb && \
-wget https://aka.ms/iotedged-linux-armhf-latest -o iotedge.deb && dpkg -i ./iotedge.deb && rm ./iotedge.deb
-
-apt-get update && apt-get install -f
+apt-get install iotedge
 
 cp rund.arm32v7.sh rund.sh
 
