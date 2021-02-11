5ee1e407c0a2d1e2d8e29fbba8eff342b033fd6f
Spencer McDonough <shmcdono@usc.edu>
2021-02-11 12:21:11 -0800

    change add native runtime rund.sh with native host config

---
+++ runtime/arm/rund.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo '=> creating config.yaml'
+cat <<EOF > /etc/iotedge/config.yaml
+provisioning:
+  source: "manual"
+  device_connection_string: "$IOT_DEVICE_CONNSTR"
+agent:
+  name: "edgeAgent"
+  type: "docker"
+  env: {}
+  config:
+    image: "mcr.microsoft.com/azureiotedge-agent:1.0"
+    auth: {}
+hostname: "my_edge_host"
+connect:
+  management_uri: "unix:///var/run/iotedge/mgmt.sock"
+  workload_uri: "unix:///var/run/iotedge/workload.sock"
+listen:
+  management_uri: "fd://iotedge.mgmt.socket"
+  workload_uri: "fd://iotedge.socket"
+homedir: "/var/lib/iotedge"
+moby_runtime:
+  uri: "unix:///var/run/docker.sock"
+EOF
+
+cat /etc/iotedge/config.yaml
+
+echo '=> running iotedge daemon'
+exec iotedged -c /etc/iotedge/config.yaml
+
