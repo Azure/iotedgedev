a0cef647a0e626d4df6e40932e95e580906de333
Cindy Deng <cindydeng@microsoft.com>
2021-04-27 11:55:47 -0700

    edit requirements.txt and Development Status

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
     keywords='azure iot edge dev tool',
     python_requires='>=3.6, <3.8',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
