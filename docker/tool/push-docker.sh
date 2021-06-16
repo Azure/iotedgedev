8f1ca028e790769e3f8657ae4a1c879ccb1c1177
Mariana Mihova <10135329+marianan@users.noreply.github.com>
2021-06-16 10:42:30 -0700

    Push iotedgedev container to MCR (#478)
    * Push iotedgedev container to MCR
    
    * Update script to reflect publishing instructions
    
    * update version detection logic

--- docker/tool/push-docker.sh
+++ docker/tool/push-docker.sh
@@ -3,57 +3,33 @@
 # stop on error
 set -e
 
-# make sure we're in docker folder
-original_folder=$PWD
-
-if [ -z $"echo $PWD | grep /docker$" ]; then 
-    in_docker_folder=1
-else
-    in_docker_folder=0
-    cd docker
-fi
-
 function show_help
 {
     echo "Usage:"
-    echo "push-docker.sh <dockerhub> <platform> [<version>]"
+    echo "push-docker.sh [<version>]"
     echo ""
-    echo "dockerhub: docker hub name (eg:microsoft/iotedgedev) used for pushing created images"    
-    echo "platform: linux|windows|empty"
     echo "version: version to push. Automatically detected if not specified"
     exit 1
 }
 
-IMAGE_NAME="$1"
-PLATFORM="$2"
-VERSION="$3"
+# iotedgetoolscontainerregistry.azure.io is the ACR that has a webhook to publish to MCR
+# only this ACR should be used
+ACR_LOGIN_SERVER="iotedgetoolscontainerregistry.azurecr.io"
+IMAGE_NAME="iotedgedev"
+VERSION="$1"
 
-if [ "$IMAGE_NAME" = "--help" ]; then    
-    echo "Usage:"
-    echo "push-docker.sh imagename [linux|windows]"
-    exit 1
+if [ "$VERSION" = "--help" ]; then    
+    show_help
 fi
 
 if [ -z "$VERSION" ]; then
     echo -e "\n===== Detecting version"
-    VERSION=$(cat ../iotedgedev/__init__.py | grep '__version__' | grep -oP "'\K[^']+")
+    VERSION=$(grep '__version__' ../../iotedgedev/__init__.py | grep -oP "'\K[^']+")
     echo "Detected version $VERSION"
 fi
 
-docker push $IMAGE_NAME:$VERSION-amd64 
-docker push $IMAGE_NAME:latest-amd64 
-
-#TODO add windows container support.  For now we only push linux.
-#docker push $IMAGE_NAME:$VERSION-windows-amd64
-#docker push $IMAGE_NAME:latest-windows-amd64
-
-echo -e "\n===== Creating Multi-Arch Docker image"
-#docker manifest create $IMAGE_NAME:latest $IMAGE_NAME:latest-amd64 $IMAGE_NAME:latest-windows-amd64 
-docker manifest create --insecure $IMAGE_NAME:latest $IMAGE_NAME:latest-amd64 
-
-echo -e "\n===== Pushing Docker Multi-Arch image"
-docker manifest push --purge $IMAGE_NAME:latest
-
-if [ in_docker_folder = 0 ]; then
-    cd original_folder
-fi
+echo -e "\n===== Pushing Docker images"
+docker push $ACR_LOGIN_SERVER/public/iotedge/$IMAGE_NAME:$VERSION-amd64
+docker push $ACR_LOGIN_SERVER/public/iotedge/$IMAGE_NAME:$VERSION
+docker push $ACR_LOGIN_SERVER/public/iotedge/$IMAGE_NAME:latest-amd64
+docker push $ACR_LOGIN_SERVER/public/iotedge/$IMAGE_NAME:latest
