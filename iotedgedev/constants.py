ca602cb791cf0707ebb04d5a35233e6deebe1b50
Cindy Deng <cindydeng@microsoft.com>
2021-08-13 17:42:45 +0000

    update schema validation template version

--- iotedgedev/constants.py
+++ iotedgedev/constants.py
@@ -7,6 +7,6 @@ class Constants:
     deployment_template_suffix = ".template.json"
     deployment_template_schema_version = "1.0.0"
     moduledir_placeholder_pattern = r'\${MODULEDIR<(.+)>(\..+)?}'
-    deployment_template_schema_url = "http://json.schemastore.org/azure-iot-edge-deployment-template-2.0"
-    deployment_manifest_schema_url = "http://json.schemastore.org/azure-iot-edge-deployment-2.0"
-    azure_cli_iot_ext_source_url = "https://github.com/Azure/azure-iot-cli-extension/releases/download/v0.9.0/azure_iot-0.9.0-py2.py3-none-any.whl"
+    deployment_template_schema_url = "http://json.schemastore.org/azure-iot-edge-deployment-template-4.0"
+    deployment_manifest_schema_url = "http://json.schemastore.org/azure-iot-edge-deployment-4.0"
+    azure_cli_iot_ext_source_url = "https://github.com/Azure/azure-iot-cli-extension/releases/download/v0.10.11/azure_iot-0.10.11-py3-none-any.whl"
