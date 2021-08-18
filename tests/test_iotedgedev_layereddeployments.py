d48b58c331ae094d1ecc63aa7a0b4baf54810aa6
cindydeng1998 <xinze.deng@gmail.com>
2021-08-18 15:38:48 -0700

    more unit test

--- tests/test_iotedgedev_layereddeployments.py
+++ tests/test_iotedgedev_layereddeployments.py
@@ -15,7 +15,6 @@ test_solution_shared_lib_dir = os.path.join(os.getcwd(), "tests", "assets", "tes
 
 output = Output()
 envvars = EnvVars(output)
-envvars.load()
 
 def test_build_and_push():
     os.chdir(test_solution_shared_lib_dir)
@@ -42,5 +41,5 @@ def test_build_and_push_with_no_modules():
     set_property = content["content"]["modulesContent"]["exampleModule"]["properties.desired"]["foo"]
 
     assert 'ERROR' not in result.output
-    assert 'bar-{}'.format(envvars.EDGE_RUNTIME_VERSION) == set_property
-    assert 'bar-{}'.format(envvars.get_envvar("EDGE_RUNTIME_VERSION")) == set_property
+    assert 'bar-${EDGE_RUNTIME_VERSION}' == set_property
+    
