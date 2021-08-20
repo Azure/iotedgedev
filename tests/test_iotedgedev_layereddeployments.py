0005d6d3cb49d6ae408fe52256f6fb0f4af82a46
Eliise <elseling@microsoft.com>
2021-08-20 13:43:35 +0000

    Add an additional test case

--- tests/test_iotedgedev_layereddeployments.py
+++ tests/test_iotedgedev_layereddeployments.py
@@ -19,10 +19,15 @@ test_solution_shared_lib_dir = os.path.join(os.getcwd(), "tests", "assets", "tes
 
 
 def test_build_and_push():
+    # Arrange
     os.chdir(test_solution_shared_lib_dir)
+    new_config_deployment_path = os.path.join(test_solution_shared_lib_dir, 'config', "layered_deployment.flattened_props.json")
 
-    result = runner_invoke(['build', '--push', '-f', "layered_deployment.template_with_flattened_props.json", '-P', get_platform_type()])
+    # Act
+    result = runner_invoke(['build', '--push', '-f', "layered_deployment.flattened_props.template.json", '-P', get_platform_type()])
 
+    # Assert
+    assert os.path.exists(new_config_deployment_path)
     assert 'sample_module:0.0.1-RC' in result.output
     assert 'BUILD COMPLETE' in result.output
     assert 'PUSH COMPLETE' in result.output
@@ -30,12 +35,14 @@ def test_build_and_push():
 
 
 def test_build_and_push_with_no_modules():
+    # Arrange
     os.chdir(test_solution_shared_lib_dir)
+    new_config_deployment_path = os.path.join(test_solution_shared_lib_dir, 'config', 'layered_deployment.no_modules.json')
 
-    result = runner_invoke(['build', '--push', '-f', "layered_deployment.template_with_no_modules.json", '-P', get_platform_type()])
+    # Act
+    result = runner_invoke(['build', '--push', '-f', "layered_deployment.no_modules.template.json", '-P', get_platform_type()])
 
-    new_config_deployment_name = 'layered_deployment.template_with_no_modules.json'
-    new_config_deployment_path = os.path.join(test_solution_shared_lib_dir, 'config', new_config_deployment_name)
+    # Assert
 
     with open(new_config_deployment_path, "r") as f:
         content = json.load(f)
@@ -49,11 +56,12 @@ def test_build_and_push_with_no_modules():
 @pytest.mark.parametrize(
     "config",
     [
-        "layered_deployment.template_with_flattened_props.json",
-        "layered_deployment.template_with_no_modules.json"
+        "layered_deployment.flattened_props.template.json",
+        "layered_deployment.no_modules.template.json",
+        "deployment.template.json"
     ]
 )
-def test_deploy(config):
+def test_deployment(config):
     try:
         # Arrange
         deployment_name = f'test-{uuid.uuid4()}'
@@ -64,7 +72,7 @@ def test_deploy(config):
         # Act
         result = runner_invoke(['build', '--push', '-f', config, '-P', get_platform_type()])
         result = runner_invoke(['deployment',
-                                '-f', f'config/{config}',
+                                '-f', f'config/{config.replace("template.", "")}',
                                 '-n', deployment_name,
                                 '-p', '10',
                                 '-t', "tags.environment='dev'"
@@ -75,23 +83,23 @@ def test_deploy(config):
         assert 'ERROR' not in result.output
     finally:
         # Cleanup
-        azure_cli = AzureCli(output, envvars)
 
         del os.environ["LAYERED_DEPLOYMENT_TARGET_CONDITION"]
-        assert azure_cli.invoke_az_cli_outproc(["iot", "edge", "deployment", "delete", "-d", deployment_name, "-l", envvars.get_envvar("IOTHUB_CONNECTION_STRING")])
+    azure_cli = AzureCli(output, envvars)
+    assert azure_cli.invoke_az_cli_outproc(["iot", "edge", "deployment", "delete", "-d", deployment_name, "-l", envvars.get_envvar("IOTHUB_CONNECTION_STRING")])
 
 
-def test_deploy_with_target_group_set_from_dotenv():
+def test_deployment_with_target_group_set_from_dotenv():
     try:
         # Arrange
-        deployment_name = f'test-{uuid.uuid4()}'
+        deployment_name = f'test-layered{uuid.uuid4()}'
         os.chdir(test_solution_shared_lib_dir)
         envvars.set_envvar("LAYERED_DEPLOYMENT_TARGET_CONDITION", "tags.environment='dev'")
 
         # Act
-        result = runner_invoke(['build', '--push', '-f', "layered_deployment.template_with_flattened_props.json", '-P', get_platform_type()])
+        result = runner_invoke(['build', '--push', '-f', "layered_deployment.flattened_props.template.json", '-P', get_platform_type()])
         result = runner_invoke(['deployment',
-                                '-f', 'config/layered_deployment.template_with_flattened_props.json',
+                                '-f', 'config/layered_deployment.flattened_props.json',
                                 '-n', deployment_name,
                                 '-p', '10',
                                 ])
@@ -107,16 +115,15 @@ def test_deploy_with_target_group_set_from_dotenv():
         assert azure_cli.invoke_az_cli_outproc(["iot", "edge", "deployment", "delete", "-d", deployment_name, "-l", envvars.get_envvar("IOTHUB_CONNECTION_STRING")])
 
 
-def test_deploy_error_from_az_cli_bubbled_up():
+def test_deployment_error_from_az_cli_bubbled_up():
     # Arrange
-    deployment_name = f'test-{uuid.uuid4()}'
     os.chdir(test_solution_shared_lib_dir)
 
     # Act
-    result = runner_invoke(['build', '--push', '-f', "layered_deployment.template_with_flattened_props.json", '-P', get_platform_type()])
+    result = runner_invoke(['build', '--push', '-f', "layered_deployment.flattened_props.template.json", '-P', get_platform_type()])
     result = runner_invoke(['deployment',
-                            '-f', 'config/layered_deployment.template_with_flattened_props.json',
-                            '-n', deployment_name,
+                            '-f', 'config/layered_deployment.flattened_props.json',
+                            '-n', "test-layered-deployment",
                             '-p', '10',
                             '-t', "invalid_target_group"
                             ])
@@ -125,7 +132,7 @@ def test_deploy_error_from_az_cli_bubbled_up():
     assert "ERROR: {'Message': 'ErrorCode:ArgumentInvalid;BadRequest'," in result.output
 
 
-def test_deploy_error_missing_name():
+def test_deployment_error_missing_name():
     with pytest.raises(Exception) as context:
         # Act
         runner_invoke(['deployment', '-n', "test"])
@@ -134,7 +141,7 @@ def test_deploy_error_missing_name():
     assert "Error: Missing option '--priority' / '-p'." in str(context)
 
 
-def test_deploy_error_missing_priority():
+def test_deployment_error_missing_priority():
     with pytest.raises(Exception) as context:
         # Act
         runner_invoke(['deployment', '-n', "test"])
@@ -143,7 +150,7 @@ def test_deploy_error_missing_priority():
     assert "Error: Missing option '--priority' / '-p'." in str(context)
 
 
-def test_deploy_error_missing_target_condition():
+def test_deployment_error_missing_target_condition():
 
     with pytest.raises(Exception) as context:
         # Act
