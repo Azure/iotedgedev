3d1029dae44cf6300c10837138dfd6db13824636
Fumihiko Shiroyama <fu.shiroyama@gmail.com>
2022-05-17 21:41:32 +0000

    Update azure-cli-core
    The azure-cli-core dependency is currently pinned to version 2.25.0,
    which is quite old, so it should be updated. Apart from that, if we want
    to install azure-cli when within the same environment using pip, the
    same version of azure-cli needs to be used. This version has a
    dependency on an old library that makes use of a deprecated feature of
    setuptools. Therefore, if we want to set the development environment
    this way, we need to use an old setuptools library.
    
    azure-cli-core should be pinned to version 2.34.1. Please note >=2.35.0
    cannot be used as this version is not compatible with the docker
    dependency, since docker requires websocket-client==0.56.0 and
    azure-cli-core>=2.35.0 requires websocket-client==1.31.1.
    
    Please note we removed the manifest for no_modules and the related test
    cases because the validation of iot edge deployment create got stricter
    with the current version and otherwise the test failed.
    
    Fixes: #560
    Co-authored-by: Isabel Lopez Andrade <beandrad@microsoft.com>

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [
     'python-dotenv',
     'requests >= 2.20.0, <= 2.25.1',
     'fstrings',
-    'azure-cli-core >= 2.25.0',
+    'azure-cli-core >= 2.34.1, < 2.35.0',
     'iotedgehubdev == 0.14.14',
     'applicationinsights == 0.11.9',
     'commentjson == 0.9.0',
@@ -31,7 +31,7 @@ test_requirements = [
 
 setup(
     name='iotedgedev',
-    version='3.3.4',
+    version='3.3.5',
     description='The Azure IoT Edge Dev Tool greatly simplifies the IoT Edge development process by automating many routine manual tasks, such as building, deploying, pushing modules and configuring the IoT Edge Runtime.',
     long_description='See https://github.com/azure/iotedgedev for usage instructions.',
     author='Microsoft Corporation',
